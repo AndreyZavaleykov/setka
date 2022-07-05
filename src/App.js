7e05442c334f16605099db8c639387cae68e5c2e
andrey.zavaleykov <apacer631@gmail.com>
2022-07-05 23:09:20 +0300

    firstReactProject

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <ul>
+                <li>css</li>
+                <li>HTML</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+};
+
+export default App;
